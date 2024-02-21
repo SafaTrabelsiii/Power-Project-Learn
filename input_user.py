aaa398c86339136da3ca74871504b281c04346c8
Safa Trabelsi <96550433+SafaTrabelsiii@users.noreply.github.com>
2024-02-21 20:02:27 +0100

    Add files via upload

---
+++ input_user.py
@@ -0,0 +1,5 @@
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+location = input('enter your location: ')
+
+print ("your name is:", name, ", your age is:", age, " you live i", location)
